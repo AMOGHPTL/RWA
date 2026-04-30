2601fea181c139784f2e2155507c18228c572c16
AMOGHPTL <amoghp549@gmail.com>
2026-04-30 18:16:50 +0530

    deploying the contract

--- functions/configs/alpacaMintConfig.js
+++ functions/configs/alpacaMintConfig.js
@@ -1,9 +1,11 @@
 import fs from "fs";
+import dotenv from "dotenv";
 import {
   Location,
   ReturnType,
   CodeLanguage,
 } from "@chainlink/functions-toolkit";
+dotenv.config();
 
 const requestConfig = {
   source: fs.readFileSync("./functions/sources/alpacaBalance.js").toString(),
--- script/DeployDTsla.s.sol
+++ script/DeployDTsla.s.sol
@@ -1,4 +1,4 @@
-// License-Identifier: MIT
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
 import {Script, console} from "forge-std/Script.sol";
